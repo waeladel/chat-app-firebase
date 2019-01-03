4c84a9e3830b4a901ebaa7a486ad73cf87916ef4
Wael Adel Sabry <adel.wael@gmail.com>
2019-01-03 08:10:47 +0200

    Check if user is signed in

--- app/src/main/java/com/trackaty/chat/MainActivity.java
+++ app/src/main/java/com/trackaty/chat/MainActivity.java
@@ -1,15 +1,28 @@
 package com.trackaty.chat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = MainActivity.class.getSimpleName();
     private TextView mTextMessage;
+    private static final int RC_SIGN_IN = 123;
+
+    //initialize the FirebaseAuth instance
+    private FirebaseAuth  mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +52,52 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Log.i(TAG, "MainActivity onCreate");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //initialize the AuthStateListener method
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Log.d(TAG, "onAuthStateChanged:signed_in_getDisplayName:" + user.getDisplayName());
+                    Log.d(TAG, "onAuthStateChanged:signed_in_getEmail():" + user.getEmail());
+                    Log.d(TAG, "onAuthStateChanged:signed_in_getPhotoUrl():" + user.getPhotoUrl());
+                    Log.d(TAG, "onAuthStateChanged:signed_in_emailVerified?:" + user.isEmailVerified());
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    //initiateLogin();
+                }
+            }
+        };
+    }//End of onCreate
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        Log.i(TAG, "MainActivity onStart");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "MainActivity onStop");
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onPause () {
+        super.onPause ();
+        Log.i(TAG, "MainActivity onPause");
     }
 
 }
