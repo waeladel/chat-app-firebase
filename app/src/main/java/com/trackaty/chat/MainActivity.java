5ebbedc807a269993f89b9665183127868dfc665
Wael Adel Sabry <adel.wael@gmail.com>
2019-01-03 20:26:11 +0200

    Register and login

--- app/src/main/java/com/trackaty/chat/MainActivity.java
+++ app/src/main/java/com/trackaty/chat/MainActivity.java
@@ -3,6 +3,14 @@ package com.trackaty.chat;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -11,15 +19,20 @@ import com.google.firebase.auth.FirebaseUser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
     private final static String TAG = MainActivity.class.getSimpleName();
     private TextView mTextMessage;
     private static final int RC_SIGN_IN = 123;
-
+    private boolean isFirstloaded; // boolean to check if back button is clicked on startActivityForResult
     //initialize the FirebaseAuth instance
     private FirebaseAuth  mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -42,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         }
+
+
     };
 
     @Override
@@ -52,9 +67,10 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        Log.i(TAG, "MainActivity onCreate");
+        Log.d(TAG, "MainActivity onCreate");
 
         mAuth = FirebaseAuth.getInstance();
+        isFirstloaded = true; // first time to open the app
 
         //initialize the AuthStateListener method
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -72,23 +88,81 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
-                    //initiateLogin();
+                    initiateLogin(); // start login activity
                 }
             }
         };
+
+
     }//End of onCreate
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_edit_profile:
+                Log.d(TAG, "MenuItem = 0");
+
+                break;
+            case R.id.action_menu_invite:
+                Log.d(TAG, "MenuItem = 1  INVITE clicked ");
+                break;
+            case R.id.action_log_out:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d(TAG, "MenuItem = 3");
+                            }
+                        });
+                /*FirebaseAuth.getInstance().signOut(); // logout firebase user
+                LoginManager.getInstance().logOut();// logout from facebook too
+                Twitter.logOut(); // logout from twitter too
+                // Google sign out
+               Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                        new ResultCallback<Status>() {
+                            @Override
+                            public void onResult(@NonNull Status status) {
+                                //updateUI(null);
+                                Log.d(TAG, "Google sign out succeeded");
+                            }
+                        });*/
+                break;
+        }
+
+        //noinspection SimplifiableIfStatement
+        /*if (id == R.id.action_settings) {
+            return true;
+        }*/
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-        Log.i(TAG, "MainActivity onStart");
+        if(isFirstloaded){ // only add the Listener when loaded for the first time
+            mAuth.addAuthStateListener(mAuthListener);
+        }
+        Log.d(TAG, "MainActivity onStart");
+        Log.d(TAG, "mAuthListener="+ mAuthListener);
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Log.i(TAG, "MainActivity onStop");
+        Log.d(TAG, "MainActivity onStop");
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
@@ -97,7 +171,91 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onPause () {
         super.onPause ();
-        Log.i(TAG, "MainActivity onPause");
+        Log.d(TAG, "MainActivity onPause");
     }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        //showMessage(getString(R.string.google_play_services_error));
+        Toast.makeText(MainActivity.this, getString(R.string.google_play_services_error),
+                Toast.LENGTH_LONG).show();
+        // Sending failed or it was canceled
+    }
+
+    private void initiateLogin() {
+
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+                //new AuthUI.IdpConfig.GoogleBuilder().build(),
+                //new AuthUI.IdpConfig.TwitterBuilder().build(),
+                //new AuthUI.IdpConfig.FacebookBuilder().build()
+
+        );
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.mipmap.ic_launcher)      // Set logo drawable
+                        .setAlwaysShowSignInMethodScreen(true)
+                        //.setTheme(R.style.Background_FirebaseUI)      // Set theme
+                        .setTosAndPrivacyPolicyUrls("https://sites.google.com/view/pray-4-mo/home","https://sites.google.com/view/pray-4-mo/home")
+                        .build(),
+                RC_SIGN_IN);
+
+        isFirstloaded = false;
+
+    }
+
+    //Activity result after user selects a provider he wants to use
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            Log.d(TAG, "requestCode ok:" + requestCode);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                Log.d(TAG, "Sign in successfully:" + response);
+                isFirstloaded = true; // to add the Listener because it won't be added Automatically on onStart
+                mAuth.addAuthStateListener(mAuthListener); //
+                //finish();
+            } else {
+                // Sign in failed, check response for error code
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(MainActivity.this, getString(R.string.sign_in_cancelled),
+                            Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "Sign in has been cancelled:" + response);
+                    if(!isFirstloaded){
+                        finish();
+                    }
+                    return;
+                }
+
+                if (ErrorCodes.NO_NETWORK == response.getError().getErrorCode()) {
+                    Log.d(TAG, "No internet connection:" + response);
+
+                    Toast.makeText(MainActivity.this, getString(R.string.no_internet_connection),
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Log.d(TAG, "Unknown error occurred:" + response);
+
+                Toast.makeText(MainActivity.this, getString(R.string.unknown_error),
+                        Toast.LENGTH_LONG).show();
+
+                Log.e(TAG, "Sign-in error: ", response.getError());
+            }
+        }
+    }
+
 
 }
